9ea3147b36a40a4f310a0b05315fa87a16c7ff6f
Andrew Aylett <andrew@aylett.co.uk>
2024-01-09 20:39:24 +0000

    Expose the Expect type from index.ts
    Signed-off-by: Andrew Aylett <andrew@aylett.co.uk>

--- test/packages.test.ts
+++ test/packages.test.ts
@@ -1,15 +1,7 @@
 import { testBuild } from '../src/index.js';
 
-import type { SpawnInstructions, TestDetails } from '../src/index.js';
-
-await testBuild(
-    '.',
-    'test',
-    (details: TestDetails): Promise<SpawnInstructions> => {
-        return Promise.resolve({
-            cmd: 'npm',
-            args: ['run', 'test'],
-            cwd: details.testDirectory,
-        });
-    },
-);
+await testBuild('.', 'test', (details) => ({
+    cmd: 'npm',
+    args: ['run', 'test'],
+    cwd: details.testDirectory,
+}));
--- test/types.d.ts
+++
@@ -1,3 +0,0 @@
-import { Expect } from '../src/expect.js';
-
-export function assert(blob: string, expect: Expect): void;
